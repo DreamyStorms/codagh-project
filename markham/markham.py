7271a8f1df4dace964289c21d4e2a9356c5e5dd8
Mattias Lindahl <mattias07ss@gmail.com>
2025-10-20 22:36:45 +0200

    Add game skimming

--- markham/markham.py
+++ markham/markham.py
@@ -2,8 +2,32 @@ import os
 import sys
 import sqlite3
 from pathlib import Path
+from pathlib import PurePath
 import chess
 import chess.pgn
+
+
+def select_games(pgn_path, min_rating):
+    path = PurePath(pgn_path)
+    pgn = open(path)
+
+    con = sqlite3.connect(Path('lib') / 'data' / 'pgn' / (path.stem + ".db"))
+    cur = con.cursor()
+    if not db_table_exist(cur, "selected_games"):
+        cur.execute("CREATE TABLE selected_games(offset)")
+    
+    while True:
+        offset = pgn.tell()
+
+        headers = chess.pgn.read_headers(pgn)
+
+        if headers is None:
+            break
+
+        if headers.get("WhiteElo") > min_rating and headers.get("BlackElo") > min_rating:
+            cur.execute(f"INSERT INTO selected_games VALUES({offset})")
+            con.commit()
+    return
 
 def pgn_to_fen(game_dir): # Takes in directory to .pgn and returns a list of [board position, move]
     pgn = open(game_dir)
@@ -17,40 +41,37 @@ def pgn_to_fen(game_dir): # Takes in directory to .pgn and returns a list of [bo
         board.push(move)
     return fen
 
-def db_table_raw_exist(cur):
-    res = cur.execute("SELECT name FROM sqlite_master WHERE type ='table' and name = 'raw'")
-    if res.fetchone == None:
+def db_table_exist(cur, name):
+    res = cur.execute(f"SELECT name FROM sqlite_master WHERE type ='table' and name = '{name}'")
+    if res.fetchone() is None:
         return False
     else:
         return True
+    
+def cli():
+    raise NotImplementedError
 
-def main(): 
+
+def main():
     con = sqlite3.connect(Path('lib') / 'data' / 'data.db')
     cur = con.cursor()
-    if not db_table_raw_exist(cur):
+    if not db_table_exist(cur, "raw"):
         cur.execute("CREATE TABLE raw(board, move)")
+    
+    if len(sys.argv) < 2:
+        cli()
+        return
+    
+    operation = sys.argv[1]
 
-    match len(sys.argv):
-        case 1:
-            raise TypeError("Markham takes 1 postional argument but none were given") #No path
-        case 2:
-            print(pgn_to_fen(sys.argv[1])) 
-        case _:
-            out_to_db = False
-            folder_in = False
+    match operation:
+        case "select-games":
+            select_games(sys.argv[2], sys.argv[3])
+        case "clear-selected":
+            pass
+        case "parse-selected":
+            pass
 
-            for i in range(2, len(sys.argv) - 1):
-                match sys.argv[i]:
-                    case "-o":
-                        out_to_db = True
-                    case "-f":
-                        folder_in = True
-                    case _:
-                        raise ValueError("Unknown argument " + "\"" + sys.argv[i] + "\"") #Argument not recognized
-            if folder_in:
-                raise NotImplementedError
-            else:
-                cur.executemany("INSERT INTO raw VALUES(?, ?)", pgn_to_fen(sys.argv[1]))
-                con.commit()
-
+    
+    
 main()
