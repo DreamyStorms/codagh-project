33e9ade8a09c3c01e826e95f8dc5902551c90273
Mattias Lindahl <mattias07ss@gmail.com>
2025-10-20 17:42:49 +0200

    Fix db initialization bug

--- markham/markham.py
+++ markham/markham.py
@@ -5,12 +5,6 @@ from pathlib import Path
 import chess
 import chess.pgn
 
-con = sqlite3.connect(Path('lib') / 'data' / 'data.db')
-cur = con.cursor()
-cur.execute("SELECT name FROM sqlite_master WHERE type ='table' and name = 'raw'")
-if cur.fetchone == None:
-    cur.execute("CREATE TABLE raw(board, move)")
-
 def pgn_to_fen(game_dir): # Takes in directory to .pgn and returns a list of [board position, move]
     pgn = open(game_dir)
     game = chess.pgn.read_game(pgn)
@@ -23,12 +17,24 @@ def pgn_to_fen(game_dir): # Takes in directory to .pgn and returns a list of [bo
         board.push(move)
     return fen
 
-def main(): # Handels arguments and redirects to the specified operation 
+def db_table_raw_exist(cur):
+    res = cur.execute("SELECT name FROM sqlite_master WHERE type ='table' and name = 'raw'")
+    if res.fetchone == None:
+        return False
+    else:
+        return True
+
+def main(): 
+    con = sqlite3.connect(Path('lib') / 'data' / 'data.db')
+    cur = con.cursor()
+    if not db_table_raw_exist(cur):
+        cur.execute("CREATE TABLE raw(board, move)")
+
     match len(sys.argv):
         case 1:
-            raise TypeError("Markham takes 1 postional argument but none were given")
+            raise TypeError("Markham takes 1 postional argument but none were given") #No path
         case 2:
-            print(pgn_to_fen(sys.argv[1]))
+            print(pgn_to_fen(sys.argv[1])) 
         case _:
             out_to_db = False
             folder_in = False
@@ -40,14 +46,11 @@ def main(): # Handels arguments and redirects to the specified operation
                     case "-f":
                         folder_in = True
                     case _:
-                        raise ValueError("Unknown argument " + "\"" + sys.argv[i] + "\"")
+                        raise ValueError("Unknown argument " + "\"" + sys.argv[i] + "\"") #Argument not recognized
             if folder_in:
                 raise NotImplementedError
             else:
                 cur.executemany("INSERT INTO raw VALUES(?, ?)", pgn_to_fen(sys.argv[1]))
                 con.commit()
-
-    
-
 
 main()
