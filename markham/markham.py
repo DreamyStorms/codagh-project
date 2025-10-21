be5b1fc0e228aad97cb0b5e94bdd86de859902c9
DreamyStorms <mattias07ss@gmail.com>
2025-10-21 09:11:13 +0200

    Add type hints and comments to functions

--- markham/markham.py
+++ markham/markham.py
@@ -6,13 +6,13 @@ from pathlib import PurePath
 import chess
 import chess.pgn
 
-def connect_db_from_pgn_path(pgn_path):
+def connect_db_from_pgn_path(pgn_path: str) -> sqlite3.Connection: # Creates a db connection from the provided pgn path
     path = PurePath(pgn_path)
 
     con = sqlite3.connect(Path('lib') / 'data' / 'pgn' / (path.stem + ".db"))
     return con
 
-def select_games(pgn_path, min_rating):
+def select_games(pgn_path: str, min_rating: int) -> None: # Creates a db with offsets form games above the minimum rating i the provided pgn
     pgn = open(pgn_path)
 
     con = connect_db_from_pgn_path(pgn_path)
@@ -33,6 +33,10 @@ def select_games(pgn_path, min_rating):
             con.commit()
     return
 
+def clear_db_table(db_con: sqlite3.Connection, table_name: str) -> None: # Clears a specified table in the provided db
+    cur = db_con.cursor
+    return
+
 def pgn_to_fen(game_dir): # Takes in directory to .pgn and returns a list of [board position, move]
     pgn = open(game_dir)
     game = chess.pgn.read_game(pgn)
@@ -45,8 +49,8 @@ def pgn_to_fen(game_dir): # Takes in directory to .pgn and returns a list of [bo
         board.push(move)
     return fen
 
-def db_table_exist(cur, name):
-    res = cur.execute(f"SELECT name FROM sqlite_master WHERE type ='table' and name = '{name}'")
+def db_table_exist(cur: sqlite3.Cursor, table_name: str) -> bool: # Returns True if the table exists in the provided db
+    res = cur.execute(f"SELECT name FROM sqlite_master WHERE type ='table' and name = '{table_name}'")
     if res.fetchone() is None:
         return False
     else:
@@ -69,7 +73,7 @@ def main():
     operation = sys.argv[1]
 
     match operation:
-        case "select-games":
+        case "select-games": # (path, minnimum_rating)
             select_games(sys.argv[2], sys.argv[3])
         case "clear-selected":
             pass
