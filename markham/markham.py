97bcf31a75104ab424e217403523e4e0f2f8bb21
DreamyStorms <mattias07ss@gmail.com>
2025-10-24 13:24:55 +0200

    Add selecting games message to select_games

--- markham/markham.py
+++ markham/markham.py
@@ -60,6 +60,8 @@ def select_games(pgn_path: str, min_rating: int) -> None: # Creates a db with of
 
         print("")
     
+    print("Selecting games...")
+
     for row in tqdm(cur.execute("SELECT offset, white_elo, black_elo FROM all_games").fetchall()):
         if row[1] >= min_rating and row[2] >= min_rating:
             cur.execute(f"INSERT INTO selected_games VALUES({row[0]})")
